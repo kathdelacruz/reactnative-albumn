cce9a739bbd00c2fdb269b5a75e6fea99ba73555
Katherine De La Cruz <kdelacruz@rcp.pe>
2017-05-22 16:20:38 -0500

    Add reusable header

--- index.ios.js
+++ index.ios.js
@@ -9,7 +9,7 @@ import Header from './src/components/header';
 export default class albums extends Component {
   render() {
     return (
-      <Header />
+      <Header headerText={'Albums'} />
     );
   }
 }
